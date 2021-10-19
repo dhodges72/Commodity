25e41b8dcd648bf521927c60c76d341a5a293056
dhodges72 <dhodges72@gmail.com>
2021-10-18 19:10:00 -0600

    Update Commodity.py

--- Commodity.py
+++ Commodity.py
@@ -20,15 +20,7 @@ start =st.date_input('Start',value =pd.to_datetime('2021-01-01'))
 end =st.date_input('End',value=pd.to_datetime('today'))
 
 
-# below code can be used to calculate the relative return of an asset
-#def relativeret(df):
-  #rel= df.pct_change ()
-   #cumret=(1+rel).cumprod()-1
-    #cumret=cumret.fillna(0)
-    #return cumret
-
 if len (dropdown)>0:
     df=yf.download(dropdown,start,end)['Adj Close']
-    #df=relativeret(yf.download(dropdown,start,end)['Adj Close'])
     st.line_chart(df)
     
